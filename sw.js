5bcab6dec8383adc0e4f7a830f9e266290a16d30
valenzuela21 <vlzdavid12@outlook.com>
2021-12-16 12:27:49 -0500

    PWA web

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-importScripts('./js/sw-util.js');
+importScripts('js/sw-util.js');
 
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
@@ -7,7 +7,6 @@ const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
     //'/',
-    'pwa-app-web-hero/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
