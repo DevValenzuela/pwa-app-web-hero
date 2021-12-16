d0ed94413f098c81d65cdb6c8b8fdabe09d1cfc9
valenzuela21 <vlzdavid12@outlook.com>
2021-12-16 12:00:13 -0500

    PWA Twittor

---
+++ js/sw-util.js
@@ -0,0 +1,27 @@
+// Function Clean Caches
+function cleanCaches(cacheName, numberItems) {
+    caches.open(cacheName).then(cache => {
+        cache.keys()
+            .then(keys => {
+                if (keys.length > numberItems) {
+                    cache.delete(keys[0])
+                        .then(cleanCaches(cacheName, numberItems));
+                }
+            });
+    });
+
+}
+
+function updateCacheDynamic(dynamicCache, req, res) {
+    if (res.ok) {
+        caches.open(dynamicCache).then((cache) => {
+            cache.put(req, res.clone()).then(() => {
+                alert('hola 21')
+                cleanCaches(dynamicCache, 50);
+            });
+            return res;
+        });
+    } else {
+        return res;
+    }
+}
