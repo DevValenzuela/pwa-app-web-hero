50a286348e7ed98116704b0ed7163552af7fb71c
valenzuela21 <vlzdavid12@outlook.com>
2021-12-16 12:46:28 -0500

    PWA Web

--- js/sw-util.js
+++ js/sw-util.js
@@ -1,23 +1,8 @@
-// Function Clean Caches
-function cleanCaches(cacheName, numberItems) {
-    caches.open(cacheName).then(cache => {
-        cache.keys()
-            .then(keys => {
-                if (keys.length > numberItems) {
-                    cache.delete(keys[0])
-                        .then(cleanCaches(cacheName, numberItems));
-                }
-            });
-    });
-
-}
-
+// Function Clean Cache
 function updateCacheDynamic(dynamicCache, req, res) {
     if (res.ok) {
         caches.open(dynamicCache).then((cache) => {
-            cache.put(req, res.clone()).then(() => {
-                cleanCaches(dynamicCache, 50);
-            });
+            cache.put(req, res.clone());
             return res;
         });
     } else {
